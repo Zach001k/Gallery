a72ede8075b31c985d81e494447ded101166d66a
Zach001k <zachkamau0@gmail.com>
2021-01-25 16:35:53 +0300

    my changes

---
+++ index.js
@@ -0,0 +1,15 @@
+ const slides = document.querySelectorAll('.slide');
+
+
+ slides.forEach(slide => {
+     slide.addEventListener('click', () =>{
+         removeActiveClasses()
+         slide.classList.add('active')
+     })
+ })
+
+ function removeActiveClasses(){ 
+     slides.forEach(slide => {
+         slide.classList.remove('active')
+     })
+ }
